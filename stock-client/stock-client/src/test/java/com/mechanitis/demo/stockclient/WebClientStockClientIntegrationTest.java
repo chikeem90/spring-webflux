c615ba07f8f9baf70f5efbec1a932072b56437b6
chikeem90 <chikeem90@gmail.com>
2020-12-25 22:26:24 +0900

    initial commit

---
+++ stock-client/stock-client/src/test/java/com/mechanitis/demo/stockclient/WebClientStockClientIntegrationTest.java
@@ -0,0 +1,23 @@
+package com.mechanitis.demo.stockclient;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+
+class WebClientStockClientIntegrationTest {
+    private WebClient webClient = WebClient.builder().build();
+
+    @Test
+    public void shouldRetrieveStockPricesFromTheService() {
+        // given
+        WebClientStockClient webClientStockClient = new WebClientStockClient(webClient);
+
+        // when
+        Flux<StockPrice> prices = webClientStockClient.pricesFor("SYMBOL");
+        Assertions.assertNotNull(prices);
+        Flux<StockPrice> fivePrices = prices.take(5);
+        Assertions.assertEquals(5, fivePrices.count().block());
+        Assertions.assertEquals("SYMBOL", fivePrices.blockFirst().getSymbol());
+    }
+}
